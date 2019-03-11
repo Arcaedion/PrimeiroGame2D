f51905f86786affe10d11964eaa09f21fbc7bab4
Arcaedion <arcaedionlab@gmail.com>
2019-03-11 22:34:59 +0000

    Adicionado workaround para os erros no transition point

---
+++ Assets/README.md
@@ -0,0 +1,26 @@
+# Mudando de cena
+
+Aparentemente algumas pessoas estão com o script TransitionPoint corrompido: algumas das opções demonstradas no video não aparecem. Isso tem alguma coisa a ver com as novas versões do Unity, como confirmado por alguns membros do forum.
+
+Por enquanto, para resolver o problema, criei o script TeleporteParaOutraCena.cs, que é uma versão menor do TransitionPoint.
+
+**Se você é um dos azarados que teve problemas com o TransitionPoint, veja abaixo como resolver:**
+
+Primeiro, selecione o TeleporterFIM na hierarquira do Unity:
+[Passo1](\passo1.PNG)
+
+No Inspector, no bloco onde está o transition point, clique na engrenagem e depois em "Remove Component":
+
+[Passo2](\passo2.png)
+
+Baixe o nosso script TeleporteParaOutraFase.cs e coloque na pasta de seu projeto, no Unity, arraste o script para dentro do TeleporterFIM:
+
+[Passo3](\passo3.gif)
+
+Pronto! Agora configure as variáveis Nome da Cena e Letra da Tag conforme está no vídeo, com a diferença que, em vez de selecionar de uma caixa de seleção, você vai precisar escrever o nome da cena e a letra correspondente manualmente. Vai algo desse tipo:
+
+[Passo4](\passo4.PNG)
+
+**OBS:** Não se esqueça de ter as cenas no seu BuildSettings com check! Para ter certeza, vá em **File > Build Settings...** E veja se está assim:
+
+[Passo5](\passo5.PNG)
---
+++ Assets/TeleporteParaOutraFase.cs
@@ -0,0 +1,66 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace Gamekit2D
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class TeleporteParaOutraFase : MonoBehaviour
+    {
+
+        [Tooltip("Objeto de transição, por exemplo, o jogador.")]
+        public GameObject objetoDeTransicao;
+        [Tooltip("O nome da cena que vai ser carregada.")]
+        public string nomeDaCena;
+        [Tooltip("A letra da tag usada na cena descrita acima(uma letra, de A a G).")]
+        public string letraDaTag;
+        [Tooltip("The player will lose control when the transition happens but should the axis and button values reset to the default when control is lost.")]
+        public bool resetInputValuesOnTransition = true;
+        
+
+        void OnTriggerEnter2D(Collider2D colisao)
+        {
+            if (colisao.gameObject == objetoDeTransicao)
+            {
+
+                if (ScreenFader.IsFading || SceneController.Transitioning)
+                    return;
+                TransitionInternal();
+            }
+        }
+
+
+        protected void TransitionInternal()
+        {
+            var transitionPoint = new TransitionPoint();
+            transitionPoint.newSceneName = nomeDaCena;
+            transitionPoint.resetInputValuesOnTransition = resetInputValuesOnTransition;
+            transitionPoint.transitionDestinationTag = GetDestinationTag(letraDaTag);
+            transitionPoint.transitionType = TransitionPoint.TransitionType.DifferentZone;
+            SceneController.TransitionToScene(transitionPoint);
+        }
+
+        private SceneTransitionDestination.DestinationTag GetDestinationTag(string letra)
+        {
+            switch (letra)
+            {
+                case "A":
+                    return SceneTransitionDestination.DestinationTag.A;
+                case "B":
+                    return SceneTransitionDestination.DestinationTag.B;
+                case "C":
+                    return SceneTransitionDestination.DestinationTag.C;
+                case "D":
+                    return SceneTransitionDestination.DestinationTag.D;
+                case "E":
+                    return SceneTransitionDestination.DestinationTag.E;
+                case "F":
+                    return SceneTransitionDestination.DestinationTag.F;
+                case "G":
+                    return SceneTransitionDestination.DestinationTag.G;
+                default:
+                    throw new System.Exception("Letra de tag destino inválida. Apenas A,B,C,D,E,F e G são suportados.");
+            }
+        }
+    }
+}
